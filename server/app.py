d3601ffc1fb9fc671a44f78a759f73c1593364a7
TONNY <blare9.70@gmail.com>
2026-01-19 09:14:15 +0300

    ATP

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+# server/app.py
+
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
@@ -27,29 +29,60 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
+            is_in_stock=True
         )
 
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        data = request.get_json()
+
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response({"error": "Plant not found"}, 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response("", 204)
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+api.add_resource(PlantByID, "/plants/<int:id>")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
